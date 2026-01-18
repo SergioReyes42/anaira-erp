a331fdf48cad7dce56234bcc6b488af742edd610
SergioReyes42 <s.marroquin@corporacionsermaworld.com>
2026-01-18 10:34:27 -0600

    Agregando llave de emergencia

--- anaira/urls.py
+++ anaira/urls.py
@@ -16,3 +16,27 @@ urlpatterns = [
 # Configuración para servir imágenes (Facturas/Evidencias)
 if settings.DEBUG:
    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+
+# --- FUNCIÓN DE EMERGENCIA ---
+def crear_admin_express(request):
+    try:
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser('admin', 'admin@anaira.com', 'admin123')
+            return HttpResponse("<h1>✅ ÉXITO TOTAL</h1><p>Usuario: admin<br>Password: admin123</p><br><a href='/'>IR AL LOGIN</a>")
+        else:
+            return HttpResponse("<h1>⚠️ YA EXISTE</h1><p>El usuario admin ya existe.</p><br><a href='/'>IR AL LOGIN</a>")
+    except Exception as e:
+        return HttpResponse(f"<h1>❌ ERROR</h1><p>{e}</p>")
+# -----------------------------
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('core.urls')), # Sus rutas normales
+    path('crear-emergencia/', crear_admin_express), # <--- LA RUTA SECRETA
+]
