a62c2e0b04a18ae71907b1c7ceef792fa1c6219b
SergioReyes42 <s.marroquin@corporacionsermaworld.com>
2026-01-29 21:55:44 -0600

    Agregano gemini

--- core/ai_brain.py
+++ core/ai_brain.py
@@ -18,7 +18,7 @@ generation_config = {
 
 model = genai.GenerativeModel(
     # CAMBIO AQUÍ: Usamos el modelo que sí apareció en su lista
-    model_name="gemini-2.0-flash", 
+    model_name="gemini-2.5-flash", 
     generation_config=generation_config,
 )
 
