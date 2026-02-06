f0e94530d0de958bdad0aff635a40a627d5b63ba
SergioReyes42 <s.marroquin@corporacionsermaworld.com>
2026-02-06 16:42:46 -0600

    USUARIOS000002000000

--- inventory/views.py
+++ inventory/views.py
@@ -14,10 +14,10 @@ from core.models import Company, Warehouse
 # ==========================================
 @login_required
 def product_list(request):
-    # --- LÍNEA DE PRUEBA (BORRAR DESPUÉS) ---
-    raise Exception("¡HOLA! SI LEES ESTO, EL ARCHIVO NUEVO SÍ CARGÓ")
-    # ----------------------------------------
-    
+    # --- PRUEBA DE VIDA (ESTO ROMPERÁ LA PÁGINA) ---
+    raise Exception("🛑 ¡SERVIDOR ACTUALIZADO! (Ahora borra esta línea)")
+    # -----------------------------------------------
+
     company_id = request.session.get('company_id')
     if not company_id: return redirect('select_company')
     
