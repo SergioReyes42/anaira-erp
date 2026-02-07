35b7f62417053677646d9bcf9d799000a769e38b
SergioReyes42 <s.marroquin@corporacionsermaworld.com>
2026-02-06 22:01:45 -0600

    inventari0000000000000o

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,17 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Productos
-    path('products/', views.product_list, name='product_list'),
-    path('products/create/', views.product_create, name='product_create'),
+    # Ruta principal (/inventario/)
+    path('', views.product_list, name='product_list'),
     
-    # anaira.com/inventario/nuevo/
+    # 🚨 ESTA ES LA LÍNEA QUE LE FALTA A SU ARCHIVO:
+    path('smart-hub/', views.smart_hub, name='smart_hub'),
+
+    # Rutas para evitar errores de enlaces rotos
     path('nuevo/', views.product_create, name='product_create'),
-    
-    # Existencias
     path('existencias/', views.stock_list, name='stock_list'),
-    
-    # Movimientos (Kardex)
-    path('movements/', views.movement_list, name='movement_list'),
+    path('movimientos/', views.movement_list, name='movement_list'),
     path('movimientos/nuevo/', views.create_movement, name='create_movement'),
 ]
