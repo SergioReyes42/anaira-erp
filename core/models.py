b6ab018156c7f7aee383ec385cbf7e5b52aff1d5
SergioReyes42 <s.marroquin@corporacionsermaworld.com>
2026-02-28 00:20:53 -0600

    Selecion de empresa0

--- core/models.py
+++ core/models.py
@@ -61,6 +61,8 @@ class UserProfile(models.Model):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
     phone = models.CharField(max_length=20, null=True, blank=True)
 
+    empresas_asignadas = models.ManyToManyField(Company, blank=True, related_name='usuarios_permitidos')
+    
     def __str__(self):
         return f"Perfil de {self.user}"
 
